f698204fc35669c974df481e151ace660094b76b
sand123 <sand123@users.noreply.github.com>
2022-01-06 17:29:29 +0500

    add new_user_identity_mod

---
+++ new_user_identity_mod/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+// The id of the address book to use to automatically set a
+// user's full name in their new identity. (This should be an
+// string, which refers to the $config['ldap_public'] array.)
+$config['new_user_identity_addressbook'] = 'local_users';
